f35fa66274489f05467260080e4d1dfce1dc4920
Anousone Kaseumsouk <anousonekaseumsouk@gmail.com>
2018-09-14 08:25:58 -0700

    Remove background component and moved background-image to App.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,35 @@
 import React from 'react';
 import Header from './Header';
 import BeerList from './BeerList';
-import Background from './Background';
 import Error404 from './Error404';
 import { Switch, Route } from 'react-router-dom';
 import NewBeer from './NewBeer';
 import AllBeer from './AllBeer';
+import bg from './../img/taps.jpg';
 
 function App(){
 
 
   return (
+    <div>
+      <style jsx>{`
+        body {
+          background-image: url(${bg});
+          background-size: cover;
+          background-repeat: no-repeat;
+          background-attachment: fixed;
+          background-clip: content-box;
+        }
+        `}</style>
     <div className="Row">
       <div className="col-md-8">
-        <Background/>
         <Header/>
       </div>
       <div className="col-md-4">
         <BeerList/>
-        
       </div>
     </div>
+  </div>
   );
 }
 
